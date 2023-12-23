e0a73976c21c859329a6ab72e87f0cfdbc2403d7
JINHYEONG KIM <sgo5178@kyonggi.ac.kr>
2023-12-23 19:51:49 +0900

    8주차 기본 문제 성공

--- Week8/Q11403.java
+++ Week8/Q11403.java
@@ -1,2 +1,52 @@
-package Week8;public class Q11403 {
+package Week8;
+
+import java.io.*;
+import java.util.StringTokenizer;
+
+public class Q11403 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        int N = Integer.parseInt(br.readLine());
+
+        int[][] matrix = new int[N][N];
+
+        for(int i=0; i < N; i++){
+            StringTokenizer st = new StringTokenizer(br.readLine()); //이게 여기 있어야 오류가 안남
+            for(int j=0; j <N; j++){
+                matrix[i][j] = Integer.parseInt(st.nextToken());
+            }
+        }
+
+        // 플로이드 와샹 알고리즘
+        // 참고 링크 1: https://m.blog.naver.com/PostView.nhn?blogId=ndb796&logNo=221234427842&proxyReferer=https:%2F%2Fwww.google.com%2F
+        // 참고 링크 2: https://steady-coding.tistory.com/94
+        // i에서 j까지 갈 수 있는가?
+        // i에서 k로 가고, k에서 j로 갈 수 있는가?
+        // 위에 2개의 질문은 동일함.
+        for (int k = 0; k < N; k++) {
+            for (int i = 0; i < N; i++) {
+                for (int j = 0; j < N; j++) {
+                    // 단순히 갈 수 있는 경로가 있는지만 체크함.
+                    if (matrix[i][k] == 1 && matrix[k][j] == 1) {
+                        matrix[i][j] = 1;
+                    }
+                }
+            }
+        }
+
+        StringBuilder sb = new StringBuilder();
+        for(int i=0; i<N; i++){
+            for(int j =0; j<N; j++){
+                sb.append(matrix[i][j] + " ");
+            }
+            sb.append("\n");
+        }
+
+        bw.write(sb.toString());
+        bw.flush();
+        bw.close();
+        br.close();
+    }
 }
--- Week8/Q1389.java
+++ Week8/Q1389.java
@@ -1,2 +1,85 @@
-package Week8;public class Q1389 {
+package Week8;
+
+import java.io.*;
+import java.util.StringTokenizer;
+
+public class Q1389 {
+
+    static final int INF = 987654321;
+
+    /*
+    * 무방향, 연결 그래프이다.
+    * */
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st;
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        st = new StringTokenizer(br.readLine());
+
+        int N = Integer.parseInt(st.nextToken());
+        int M = Integer.parseInt(st.nextToken());
+
+        int [][] matrix = new int[N+1][N+1]; // 입력값이 인덱스 값보다 1씩 크므로
+
+
+        // 초기값 설정을 꼭 INF로 해줘야 하는지? 그냥 0으로 하면 안되나?
+        // 최단 거리를 갱신해야하므로 초기화해줘야 한다.
+
+        // 초기값 설정
+        for (int i = 1; i <= N; i++) {
+            for (int j = 1; j <= N; j++) {
+                matrix[i][j] = INF;
+
+                if (i == j) {
+                    matrix[i][j] = 0;
+                }
+            }
+        }
+
+
+        for(int i =0; i<M; i++){
+            st = new StringTokenizer(br.readLine());
+
+            int A = Integer.parseInt(st.nextToken());
+            int B = Integer.parseInt(st.nextToken());
+
+            //무방향 그래프이므로 양방향
+            matrix[A][B] = 1;
+            matrix[B][A] = 1;
+        }
+
+        // 플로이드 와샬 알고리즘
+        for (int k = 1; k <= N; k++) {
+            for (int i = 1; i <= N; i++) {
+                for (int j = 1; j <= N; j++) {
+                    // 최단경로 초기화, 앞 문제 11403와 다른 부분임
+                    if (matrix[i][j] > matrix[i][k] + matrix[k][j]) {
+                        matrix[i][j] = matrix[i][k] + matrix[k][j];
+                    }
+                }
+            }
+        }
+
+        int res = INF;
+        int idx = -1;
+
+        // 케빈 베이컨의 수가 가장 작은 인덱스를 탐색
+        for (int i = 1; i <= N; i++) {
+            int total = 0; // 케빈 베이컨의 수를 세는 변수
+            for (int j = 1; j <= N; j++) {
+                total += matrix [i][j];
+            }
+
+            if (res > total) {
+                res = total;
+                idx = i;
+            }
+        }
+
+        bw.write(idx + "\n");
+        bw.flush();
+        bw.close();
+        br.close();
+    }
 }
